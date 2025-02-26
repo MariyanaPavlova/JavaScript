1c230609f782aea3ce6f7097b1b467316716575c
Mariyana Pavlova <milkova.mariyana@gmail.com>
2025-02-26 21:10:18 +0200

    Update sort.js

--- JavaScript/sort.js
+++ JavaScript/sort.js
@@ -21,3 +21,37 @@ function arrSortNames() {
 }
 arrSortNames()
 
+//Objects cannot be sorted; they must be converted first
+//Convert to array for sorting, filtering and mapping
+
+let phonebook = { 'Tim': '0876566344',
+                 'Bill': '0896543112' };
+
+let entries = Object.entries(phonebook);
+console.log(entries); // Array of arrays with two elements each
+    // [ ['Tim', '0876566344'],
+    // ['Bill', '0896543112'] ]
+let firstEntry = entries[0];
+console.log(firstEntry[0]); // Entry key -> 'Tim'
+console.log(firstEntry[1]); // Entry value -> '0876566344'
+
+----------
+function addressBook(input) {
+    let addressbook = {};
+    
+    for (let line of input) {
+        let [name, address] = line.split(':');
+        addressbook[name] = address;
+    }
+let sorted = Object.entries(addressbook);
+sorted.sort((a, b) => a[0].localeCompare(b[0])); //sort
+
+entries.sort(([keyA, valueA],[keyB, valueB]) => [keyA].localCompare([keyB]);
+
+
+}
+addressBook(
+['Tim:Doe Crossing',
+'Bill:Nelson Place',
+'Peter:Carlyle Ave',
+'Bill:Ornery Rd'])
