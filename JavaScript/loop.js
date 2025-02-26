adce179c2b0bf03b810152f1b2ae0aab09b00332
Mariyana Pavlova <milkova.mariyana@gmail.com>
2025-02-26 19:42:10 +0200

    Update loop.js

--- JavaScript/loop.js
+++ JavaScript/loop.js
@@ -13,6 +13,7 @@ for (let x of cars) {
 }
 
 //FOR IN - loops through the properties of an Object:
+//минава през key
 const person = {fname:"John", lname:"Doe", age:25};
 let text = "";
 for (let x in person) {
